111b2c1eb8314eff616e4d2303d0f026747a869b
CauaHenrique19 <cauah6002@gmail.com>
2021-12-11 01:33:51 -0300

    Liberating cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 import { router } from './routes'
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
